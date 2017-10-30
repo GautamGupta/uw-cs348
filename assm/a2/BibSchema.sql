16ad78c9fef8029a79a446dc767c0c3df80d2199
Gautam Gupta <gautam@gaut.am>
2017-10-30 01:03:52 -0400

    bibauthor, test data

--- assm/a2/BibSchema.sql
+++ assm/a2/BibSchema.sql
@@ -14,6 +14,9 @@ create table author ( \
 insert into author (aid, name) values (1, 'Peter Bumbulis')
 insert into author (aid, name, url) \
 values (2, 'Ivan T. Bowman', 'http://db.uwaterloo.ca/~itbowman')
+insert into author (aid, name, url) \
+values (3, 'Sigmund Freud', 'en.wikipedia.org/wiki/Sigmund_Freud')
+insert into author (aid, name) values (4, 'Voltaire')
 
 drop table publication
 create table publication ( \
@@ -25,6 +28,36 @@ insert into publication (pubid, title) \
 values ('SIGMOD02', 'Proc. ACM SIGMOD Conference on Management of Data')
 insert into publication (pubid, title) \
 values ('BB02', 'A Compact B-Tree')
+insert into publication (pubid, title) \
+values ('SF01', 'Constructions in Analysis')
+insert into publication (pubid, title) \
+values ('SF02', 'The Interpretation of Dreams')
+insert into publication (pubid, title) \
+values ('SF03', 'Neurosis and Psychosis')
+insert into publication (pubid, title) \
+values ('SF04', 'On Aphasia')
+insert into publication (pubid, title) \
+values ('SF05', 'On Psychotherapy')
+insert into publication (pubid, title) \
+values ('SF06', 'Moses and Monotheism')
+insert into publication (pubid, title) \
+values ('SF07', 'On Narcissism: an Introduction')
+insert into publication (pubid, title) \
+values ('SF08', 'The Unconscious')
+insert into publication (pubid, title) \
+values ('V01', 'Le Mondain')
+insert into publication (pubid, title) \
+values ('V02', 'Treatise on Tolerance')
+insert into publication (pubid, title) \
+values ('V03', 'Candide')
+insert into publication (pubid, title) \
+values ('PR01', 'The Most Fire Papers of PsyCon 07')
+insert into publication (pubid, title) \
+values ('J01', 'Pop Psych')
+insert into publication (pubid, title) \
+values ('J02', 'Pop Psych')
+insert into publication (pubid, title) \
+values ('J03', 'Crisp Rhymes for Philosophical Times')
 
 drop table wrote
 create table wrote ( \
@@ -37,6 +70,21 @@ create table wrote ( \
 
 insert into wrote (aid, pubid, aorder) values (1, 'BB02', 1)
 insert into wrote (aid, pubid, aorder) values (2, 'BB02', 2)
+insert into wrote (aid, pubid, aorder) values (3, 'SF01', 1)
+insert into wrote (aid, pubid, aorder) values (3, 'SF02', 1)
+insert into wrote (aid, pubid, aorder) values (4, 'SF02', 2)
+insert into wrote (aid, pubid, aorder) values (4, 'SF03', 1)
+insert into wrote (aid, pubid, aorder) values (3, 'SF03', 2)
+insert into wrote (aid, pubid, aorder) values (3, 'SF04', 1)
+insert into wrote (aid, pubid, aorder) values (3, 'SF05', 1)
+insert into wrote (aid, pubid, aorder) values (3, 'SF06', 1)
+insert into wrote (aid, pubid, aorder) values (4, 'SF06', 2)
+insert into wrote (aid, pubid, aorder) values (3, 'SF07', 1)
+insert into wrote (aid, pubid, aorder) values (4, 'SF08', 1)
+insert into wrote (aid, pubid, aorder) values (3, 'SF08', 2)
+insert into wrote (aid, pubid, aorder) values (4, 'V01', 1)
+insert into wrote (aid, pubid, aorder) values (4, 'V02', 1)
+insert into wrote (aid, pubid, aorder) values (4, 'V03', 1)
 
 drop table proceedings
 create table proceedings ( \
@@ -46,6 +94,7 @@ create table proceedings ( \
     foreign key (pubid) references publication (pubid) )
 
 insert into proceedings (pubid, year) values ('SIGMOD02', 2002)
+insert into proceedings (pubid, year) values ('PR01', 1908)
 
 drop table journal
 create table journal ( \
@@ -56,6 +105,10 @@ create table journal ( \
     primary key (pubid), \
     foreign key (pubid) references publication (pubid) )
 
+insert into journal (pubid, volume, number, year) values ('J01', 15, 1, 1913)
+insert into journal (pubid, volume, number, year) values ('J02', 18, 3, 1917)
+insert into journal (pubid, volume, number, year) values ('J03', 3, 1, 1737)
+
 drop table book
 create table book ( \
     pubid       char(10) not null, \
@@ -63,6 +116,13 @@ create table book ( \
     year        integer not null, \
     primary key (pubid), \
     foreign key (pubid) references publication (pubid) )
+
+insert into book (pubid, publisher, year) values ('SF02', 'Random House', 1900)
+insert into book (pubid, publisher, year) values ('SF04', 'Scholastic', 1891)
+insert into book (pubid, publisher, year) values ('SF06', 'Scholastic', 1939)
+insert into book (pubid, publisher, year) values ('SF07', 'Random House', 1914)
+insert into book (pubid, publisher, year) values ('V02', 'Scholastic', 1914)
+insert into book (pubid, publisher, year) values ('V03', 'Pearson', 1914)
 
 drop table article
 create table article ( \
@@ -76,5 +136,15 @@ create table article ( \
 
 insert into article (pubid, appearsin, startpage, endpage) \
 values ('BB02', 'SIGMOD02', 533, 541)
+insert into article (pubid, appearsin, startpage, endpage) \
+values ('SF01', 'PR01', 139, 147)
+insert into article (pubid, appearsin, startpage, endpage) \
+values ('SF03', 'J01', 342, 349)
+insert into article (pubid, appearsin, startpage, endpage) \
+values ('SF05', 'J01', 350, 361)
+insert into article (pubid, appearsin, startpage, endpage) \
+values ('SF08', 'J02', 67, 81)
+insert into article (pubid, appearsin, startpage, endpage) \
+values ('V01', 'J03', 43, 44)
 
 commit work
